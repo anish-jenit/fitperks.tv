a556381703c60b072d190a6f3ea2f45098a3e113
Anish Jenit <anish.jenit@gmail.com>
2026-09-17 06:12:10 +0800

    Trust www app origins for TV messages

--- script.js
+++ script.js
@@ -25,7 +25,9 @@ if (coachAvatar instanceof HTMLImageElement) {
 
 const TRUSTED_APP_ORIGINS = new Set([
   'https://fitperks.ai',
+  'https://www.fitperks.ai',
   'https://fitperks.org',
+  'https://www.fitperks.org',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
 ])
